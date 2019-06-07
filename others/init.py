fa735aedb713042efda3e5a0abc1102fc156d4a7
ReOneK <959512421@qq.com>
2019-06-07 21:57:42 +0800

    "modify"

--- others/init.py
+++ others/init.py
@@ -1,8 +1,38 @@
 #%%
+import torch.nn as nn
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.features = nn.Sequential(
+                    nn.Conv2d(3, 6, 5),
+                    nn.ReLU(),
+                    nn.MaxPool2d(2,2),
+                    nn.Conv2d(6, 16, 5),
+                    nn.ReLU(),
+                    nn.MaxPool2d(2,2)
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(16 * 5 * 5, 120),
+            nn.ReLU(),
+            nn.Linear(120, 84),
+            nn.ReLU(),
+            nn.Linear(84, 10)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(-1, 16 * 5 * 5)
+        x = self.classifier(x)
+        return x
+
+net = Net()
+
+#%%
 # 利用nn.init初始化
 import torch.nn as nn
 from torch.nn import init
 import torch
+
 linear = nn.Linear(3, 4)
 
 torch.manual_seed(1)
